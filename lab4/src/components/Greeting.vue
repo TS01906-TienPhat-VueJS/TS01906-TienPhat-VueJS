ceab91b91865eaff92e9bc0869647d3f89eef1d3
TS01906-TienPhat-VueJS <phatts01906@gmail.com>
2026-01-17 23:12:08 +0700

    (viết mô tả vào đây

---
+++ lab4/src/components/Greeting.vue
@@ -0,0 +1,26 @@
+<template>
+  <div style="padding:16px; text-align:center">
+    <h2>{{ greeting }}</h2>
+
+    <button :title="hint" @click="changeGreeting">
+      Click để đổi lời chào
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      greeting: "Xin chào bạn!",
+      hint: "Nhấn để đổi lời chào"
+    };
+  },
+  methods: {
+    changeGreeting() {
+      this.greeting = "Hello World!";
+      this.hint = "Đã đổi xong!";
+    }
+  }
+};
+</script>
